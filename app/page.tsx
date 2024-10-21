312f09d78440f4765945b301260783ef41b83a08
Anas Ahmed <anas.ahassan@yahoo.com>
2024-10-21 14:54:47 +0300

    Add github link to header

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import OpenAISimliInteraction from './OpenAISimliInteraction';
 import DottedFace from './DottedFace';
 import SimliHeaderLogo from './Logo';
 import Navbar from './Navbar';
+import Image from "next/image";
+import GitHubLogo from "@/media/github-mark-white.svg";
 
 const avatar = {
   name: "Frank",
@@ -24,7 +26,7 @@ const Demo: React.FC = () => {
       <SimliHeaderLogo />
       <Navbar />
       <div className="absolute top-[32px] right-[32px]">
-        <text className="font-bold mb-8 text-xl leading-8">Create OpenAI-Simli App</text>
+        <text onClick={() => {window.open("https://github.com/simliai")}} className="font-bold cursor-pointer mb-8 text-xl leading-8"><Image className='w-[20px] inline mr-2' src={GitHubLogo} alt='' />create-simli-app (OpenAI)</text>
       </div>
       <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
         <div>
