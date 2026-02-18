0a35184449f7a1a54a7fae6d6a72a08902ff27ce
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2026-02-18 15:33:08 +0100

    update simli client to latest

--- app/AvatarInteraction.tsx
+++ app/AvatarInteraction.tsx
@@ -2,232 +2,243 @@ import VideoBox from '@/app/components/VideoBox';
 import cn from '@/app/utils/TailwindMergeAndClsx';
 import IconSparkleLoader from "@/media/IconSparkleLoader";
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { SimliClient } from 'simli-client';
+import { generateIceServers, generateSimliSessionToken, LogLevel, SimliClient } from 'simli-client';
 
 interface AvatarInteractionProps {
-  simli_faceid: string;
-  elevenlabs_voiceid: string;
-  initialPrompt: string;
-  onStart: () => void;
-  showDottedFace: boolean;
+    simli_faceid: string;
+    elevenlabs_voiceid: string;
+    initialPrompt: string;
+    onStart: () => void;
+    showDottedFace: boolean;
 }
 
-const simliClient = new SimliClient();
+let simliClient: SimliClient | null = null
 
 const AvatarInteraction: React.FC<AvatarInteractionProps> = ({
-  simli_faceid,
-  elevenlabs_voiceid,
-  initialPrompt,
-  onStart,
-  showDottedFace
+    simli_faceid,
+    elevenlabs_voiceid,
+    initialPrompt,
+    onStart,
+    showDottedFace
 }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isAvatarVisible, setIsAvatarVisible] = useState(false);
-  const [error, setError] = useState('');
-  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const socketRef = useRef<WebSocket | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isAvatarVisible, setIsAvatarVisible] = useState(false);
+    const [error, setError] = useState('');
+    const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const socketRef = useRef<WebSocket | null>(null);
 
-  const initializeSimliClient = useCallback(() => {
-    if (videoRef.current && audioRef.current) {
-      simliClient.Initialize({
-        apiKey: process.env.NEXT_PUBLIC_SIMLI_API_KEY || '',
-        faceID: simli_faceid,
-        handleSilence: true,
-        maxSessionLength: 200,
-        maxIdleTime: 100,
-        videoRef: videoRef,
-        audioRef: audioRef,
-      });
-      console.log('Simli Client initialized');
-    }
-  }, [simli_faceid]);
+    const initializeSimliClient = useCallback(async () => {
+        if (!videoRef.current || !audioRef.current) {
+            console.error('initializeSimliClient: videoRef or audioRef is null');
+            return;
+        }
+        if (videoRef.current && audioRef.current) {
+            const SimliConfig = {
+                faceId: simli_faceid,
+                maxIdleTime: 600,
+                maxSessionLength: 600,
+                handleSilence: true,
+            };
 
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      setAudioStream(stream);
-    } catch (err) {
-      console.error('Error accessing microphone:', err);
-      setError('Error accessing microphone. Please check your permissions.');
-    }
-  };
+            simliClient = new SimliClient((
+                await generateSimliSessionToken(
+                    { apiKey: (process.env.NEXT_PUBLIC_SIMLI_API_KEY as string), config: SimliConfig },
+                )).session_token,
+                videoRef.current,
+                audioRef.current,
+                await generateIceServers(
+                    (process.env.NEXT_PUBLIC_SIMLI_API_KEY as string),
 
-  const initializeWebSocket = useCallback((connectionId: string) => {
-    socketRef.current = new WebSocket(`ws://localhost:8080/ws?connectionId=${connectionId}`);
+                ),
+                LogLevel.DEBUG,
+                "p2p",
+            )
+            simliClient.on('start', () => {
+                console.log('SimliClient connected');
+                setIsAvatarVisible(true);
+                const audioData = new Uint8Array(6000).fill(0);
+                simliClient?.sendAudioData(audioData);
+            });
+            await simliClient.start()
+            await startConversation();
 
-    socketRef.current.onopen = () => {
-      console.log('Connected to server');
-    };
+        }
+    }, [simli_faceid]);
 
-    socketRef.current.onmessage = (event) => {
-      if (event.data instanceof Blob) {
-        event.data.arrayBuffer().then((arrayBuffer) => {
-          const uint8Array = new Uint8Array(arrayBuffer);
-          simliClient.sendAudioData(uint8Array);
-        });
-      } else {
+    const startRecording = async () => {
         try {
-          const message = JSON.parse(event.data);
-          if (message.type === 'interrupt') {
-            console.log('Interrupting current response');
-            simliClient.ClearBuffer();
-          } else if (message.type === 'text') {
-            // const uint8Array = new Uint8Array(6000).fill(0);
-            // simliClient.sendAudioData(uint8Array);
-          }
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            setAudioStream(stream);
+        } catch (err) {
+            console.error('Error accessing microphone:', err);
+            setError('Error accessing microphone. Please check your permissions.');
+        }
+    };
+
+    const initializeWebSocket = useCallback((connectionId: string) => {
+        socketRef.current = new WebSocket(`ws://localhost:8080/ws?connectionId=${connectionId}`);
+
+        socketRef.current.onopen = () => {
+            console.log('Connected to server');
+        };
+
+        socketRef.current.onmessage = (event) => {
+            if (event.data instanceof Blob) {
+                event.data.arrayBuffer().then((arrayBuffer) => {
+                    const uint8Array = new Uint8Array(arrayBuffer);
+                    simliClient?.sendAudioData(uint8Array);
+                });
+            } else {
+                try {
+                    const message = JSON.parse(event.data);
+                    if (message.type === 'interrupt') {
+                        console.log('Interrupting current response');
+                        simliClient?.ClearBuffer();
+                    } else if (message.type === 'text') {
+                        // const uint8Array = new Uint8Array(6000).fill(0);
+                        // simliClient.sendAudioData(uint8Array);
+                    }
+                } catch (error) {
+                    console.error('Error parsing WebSocket message:', error);
+                }
+            }
+        };
+
+        socketRef.current.onerror = (error) => {
+            console.error('WebSocket error:', error);
+            setError('WebSocket connection error. Please check if the server is running.');
+        };
+    }, []);
+
+    const startConversation = useCallback(async () => {
+        try {
+            const response = await fetch('http://localhost:8080/start-conversation', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prompt: initialPrompt,
+                    voiceId: elevenlabs_voiceid
+                }),
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.error);
+            }
+
+            const data = await response.json();
+            initializeWebSocket(data.connectionId);
         } catch (error) {
-          console.error('Error parsing WebSocket message:', error);
+            console.error('Error starting conversation:', error);
+            window.alert(`Whoopsie! Encountered the following error(s):\n\n[${error}].\n\nTry fixing those and restarting the application (npm run start).`)
+            handleStop();
+            setError('Failed to start conversation. Please try again.');
         }
-      }
-    };
+    }, [elevenlabs_voiceid, initialPrompt, initializeWebSocket]);
 
-    socketRef.current.onerror = (error) => {
-      console.error('WebSocket error:', error);
-      setError('WebSocket connection error. Please check if the server is running.');
-    };
-  }, []);
+    const handleStart = useCallback(async () => {
+        setIsLoading(true);
+        setError('');
+        onStart();
+        await initializeSimliClient();
+        startRecording();
+    }, [onStart, initializeSimliClient]);
 
-  const startConversation = useCallback(async () => {
-    try {
-      const response = await fetch('http://localhost:8080/start-conversation', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt: initialPrompt,
-          voiceId: elevenlabs_voiceid
-        }),
-      });
+    const handleStop = useCallback(() => {
+        setIsLoading(false);
+        setError('');
+        setIsAvatarVisible(false);
+        setAudioStream(null);
 
-      if (!response.ok) {
-        const data = await response.json();
-        throw new Error(data.error);
-      }
-
-      const data = await response.json();
-      initializeWebSocket(data.connectionId);
-    } catch (error) {
-      console.error('Error starting conversation:', error);
-      window.alert(`Whoopsie! Encountered the following error(s):\n\n[${error}].\n\nTry fixing those and restarting the application (npm run start).`)
-      handleStop();
-      setError('Failed to start conversation. Please try again.');
-    }
-  }, [elevenlabs_voiceid, initialPrompt, initializeWebSocket]);
-
-  const handleStart = useCallback(async () => {
-    setIsLoading(true);
-    setError('');
-    onStart();
-
-    await startConversation();
-    simliClient.start();
-    startRecording();
-  }, [onStart, startConversation]);
-
-  const handleStop = useCallback(() => {
-    setIsLoading(false);
-    setError('');
-    setIsAvatarVisible(false);
-    setAudioStream(null);
-    
-    if (audioStream) {
-      audioStream.getTracks().forEach(track => track.stop());
-    }
-
-    simliClient.close();
-    socketRef.current?.close();
-    window.location.href = '/';
-  }, [audioStream]);
-
-  useEffect(() => {
-    if (simliClient) {
-      simliClient.on('connected', () => {
-        console.log('SimliClient connected');
-        setIsAvatarVisible(true);
-        const audioData = new Uint8Array(6000).fill(0);
-        simliClient.sendAudioData(audioData);
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    initializeSimliClient();
-
-    return () => {
-      if (socketRef.current) {
-        socketRef.current.close();
-      }
-      simliClient.close();
-    };
-  }, [initializeSimliClient]);
-
-  useEffect(() => {
-    if (audioStream && socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
-      const mediaRecorder = new MediaRecorder(audioStream);
-
-      mediaRecorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          socketRef.current?.send(event.data);
+        if (audioStream) {
+            audioStream.getTracks().forEach(track => track.stop());
         }
-      };
 
-      mediaRecorder.start(100);
+        simliClient?.stop();
+        simliClient = null
+        socketRef.current?.close();
+        window.location.href = '/';
+    }, [audioStream]);
 
-      return () => {
-        mediaRecorder.stop();
-      };
-    }
-  }, [audioStream]);
 
-  return (
-    <>
-      <div
-        className={`transition-all duration-300 ${showDottedFace ? "h-0 overflow-hidden" : "h-auto"
-          }`}
-      >
-        <VideoBox video={videoRef} audio={audioRef} />
-      </div>
-      <div className="flex flex-col items-center">
-        {!isAvatarVisible ? (
-          <button
-            onClick={handleStart}
-            disabled={isLoading}
-            className={cn(
-              "w-full h-[52px] mt-4 disabled:bg-[#343434] disabled:text-white disabled:hover:rounded-[100px] bg-simliblue text-white py-3 px-6 rounded-[100px] transition-all duration-300 hover:text-black hover:bg-white hover:rounded-sm",
-              "flex justify-center items-center"
-            )}
-          >
-            {isLoading ? (
-              <IconSparkleLoader className="h-[20px] animate-loader" />
-            ) : (
-              <span className="font-abc-repro-mono font-bold w-[164px]">
-                Test Interaction
-              </span>
-            )}
-          </button>
-        ) : (
-          <div className="flex items-center gap-4 w-full">
-            <button
-              onClick={handleStop}
-              className={cn(
-                "mt-4 group text-white flex-grow bg-red hover:rounded-sm hover:bg-white h-[52px] px-6 rounded-[100px] transition-all duration-300"
-              )}
+    useEffect(() => {
+        return () => {
+            if (socketRef.current) {
+                socketRef.current.close();
+            }
+            simliClient?.stop();
+            simliClient = null;
+        };
+    }, []);
+
+    useEffect(() => {
+        if (audioStream && socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+            const mediaRecorder = new MediaRecorder(audioStream);
+
+            mediaRecorder.ondataavailable = (event) => {
+                if (event.data.size > 0) {
+                    socketRef.current?.send(event.data);
+                }
+            };
+
+            mediaRecorder.start(100);
+
+            return () => {
+                mediaRecorder.stop();
+            };
+        }
+    }, [audioStream]);
+
+    return (
+        <>
+            <div
+                className={`transition-all duration-300 ${showDottedFace ? "h-0 overflow-hidden" : "h-auto"
+                    }`}
             >
-              <span className="font-abc-repro-mono group-hover:text-black font-bold w-[164px] transition-all duration-300">
-                Stop Interaction
-              </span>
-            </button>
-          </div>
-        )}
-      </div>
-      {error && (
-        <p className="mt-4 text-red-500 text-center">{error}</p>
-      )}
-    </>
-  );
+                <VideoBox video={videoRef} audio={audioRef} />
+            </div>
+            <div className="flex flex-col items-center">
+                {!isAvatarVisible ? (
+                    <button
+                        onClick={handleStart}
+                        disabled={isLoading}
+                        className={cn(
+                            "w-full h-[52px] mt-4 disabled:bg-[#343434] disabled:text-white disabled:hover:rounded-[100px] bg-simliblue text-white py-3 px-6 rounded-[100px] transition-all duration-300 hover:text-black hover:bg-white hover:rounded-sm",
+                            "flex justify-center items-center"
+                        )}
+                    >
+                        {isLoading ? (
+                            <IconSparkleLoader className="h-[20px] animate-loader" />
+                        ) : (
+                            <span className="font-abc-repro-mono font-bold w-[164px]">
+                                Test Interaction
+                            </span>
+                        )}
+                    </button>
+                ) : (
+                    <div className="flex items-center gap-4 w-full">
+                        <button
+                            onClick={handleStop}
+                            className={cn(
+                                "mt-4 group text-white flex-grow bg-red hover:rounded-sm hover:bg-white h-[52px] px-6 rounded-[100px] transition-all duration-300"
+                            )}
+                        >
+                            <span className="font-abc-repro-mono group-hover:text-black font-bold w-[164px] transition-all duration-300">
+                                Stop Interaction
+                            </span>
+                        </button>
+                    </div>
+                )}
+            </div>
+            {error && (
+                <p className="mt-4 text-red-500 text-center">{error}</p>
+            )}
+        </>
+    );
 };
 
 export default AvatarInteraction;
--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ interface Avatar {
 // Updated JSON structure for avatar data with image URLs
 const avatar: Avatar = {
   name: "Chrystal",
-  simli_faceid: "b7da5ed1-2abc-47c8-b7a6-0b018e031a26",
+  simli_faceid: "b9e5fba3-071a-4e35-896e-211c4d6eaa7b",
   elevenlabs_voiceid: "cgSgspJ2msm6clMCkdW9",
   initialPrompt:
     "You are a support agent for Simli and you're living in local Create-Simli-App, the interactive demo for Simli that you can start building from. You can swap me out with other characters.",
@@ -36,63 +36,63 @@ const Demo: React.FC = () => {
 
   return (
     <>
-    <Toaster />
-    <div className="bg-black min-h-screen flex flex-col items-center font-abc-repro font-normal text-sm text-white p-8">
-      <SimliHeaderLogo />
-      <Navbar />
-      <div className="absolute top-[32px] right-[32px]">
-        <text
-          onClick={() => {
-            window.open("https://github.com/simliai/create-simli-app");
-          }}
-          className="font-bold cursor-pointer mb-8 text-xl leading-8"
-        >
-          <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
-          create-simli-app
-        </text>
-      </div>
-      <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
-        <div>
-          {showDottedFace && <DottedFace />}
-          <AvatarInteraction
-            simli_faceid={avatar.simli_faceid}
-            elevenlabs_voiceid={avatar.elevenlabs_voiceid}
-            initialPrompt={avatar.initialPrompt}
-            onStart={onStart}
-            showDottedFace={showDottedFace}
-          />
+      <Toaster />
+      <div className="bg-black min-h-screen flex flex-col items-center font-abc-repro font-normal text-sm text-white p-8">
+        <SimliHeaderLogo />
+        <Navbar />
+        <div className="absolute top-[32px] right-[32px]">
+          <text
+            onClick={() => {
+              window.open("https://github.com/simliai/create-simli-app");
+            }}
+            className="font-bold cursor-pointer mb-8 text-xl leading-8"
+          >
+            <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
+            create-simli-app
+          </text>
         </div>
-      </div>
+        <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
+          <div>
+            {showDottedFace && <DottedFace />}
+            <AvatarInteraction
+              simli_faceid={avatar.simli_faceid}
+              elevenlabs_voiceid={avatar.elevenlabs_voiceid}
+              initialPrompt={avatar.initialPrompt}
+              onStart={onStart}
+              showDottedFace={showDottedFace}
+            />
+          </div>
+        </div>
 
-      <div className="max-w-[350px] font-thin flex flex-col items-center ">
-        <span className="font-bold mb-[8px] leading-5 ">
-          {" "}
-          Create Simli App is a starter repo for creating an interactive app
-          with Simli.{" "}
-        </span>
-        <ul className="list-decimal list-inside max-w-[350px] ml-[6px] mt-2">
-          <li className="mb-1">Fill in your API keys in the .env file.</li>
-          <li className="mb-1">
-            Test out the interaction and have a conversation with our default
-            avatar.
-          </li>
-          <li className="mb-1">
-            You can replace the avatar's face and voice and initial prompt with
-            your own. Do this by editing <code>app/page.tsx</code>.
-          </li>
-        </ul>
-        <span className=" mt-[16px]">
-          You can now deploy this app to Vercel, or incorporate it as part of
-          your existing project.
-        </span>
+        <div className="max-w-[350px] font-thin flex flex-col items-center ">
+          <span className="font-bold mb-[8px] leading-5 ">
+            {" "}
+            Create Simli App is a starter repo for creating an interactive app
+            with Simli.{" "}
+          </span>
+          <ul className="list-decimal list-inside max-w-[350px] ml-[6px] mt-2">
+            <li className="mb-1">Fill in your API keys in the .env file.</li>
+            <li className="mb-1">
+              Test out the interaction and have a conversation with our default
+              avatar.
+            </li>
+            <li className="mb-1">
+              You can replace the avatar's face and voice and initial prompt with
+              your own. Do this by editing <code>app/page.tsx</code>.
+            </li>
+          </ul>
+          <span className=" mt-[16px]">
+            You can now deploy this app to Vercel, or incorporate it as part of
+            your existing project.
+          </span>
+        </div>
+        {error && (
+          <p className="mt-6 text-red-500 bg-red-100 border border-red-400 rounded p-3">
+            {error}
+          </p>
+        )}
       </div>
-      {error && (
-        <p className="mt-6 text-red-500 bg-red-100 border border-red-400 rounded p-3">
-          {error}
-        </p>
-      )}
-    </div>
-  </>);
+    </>);
 };
 
 export default Demo;
